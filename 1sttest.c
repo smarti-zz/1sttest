924a9f03a11a7f45bf9306b94ebab36db2407960
smarti-zz <div__@t-online.de>
2021-01-05 15:04:05 +0100

    Update 1sttest.c
    Fixed 2 editing errors in src code

--- 1sttest.c
+++ 1sttest.c
@@ -2,14 +2,14 @@
 //Simple c-code for printing command-line parameters
 #include <stdio.h>
 
-int main(int arg c, char *argv[]) {
+int main(int argc, char *argv[]) {
   int i;
   char **sp = argv;
   
   for (i = 0; i < argc; i++)
     printf("argv[%d] = %s\n", i, *sp++);
   
-  puts("Ready!")
+  puts("Ready!");
   
   return 0;
 }
