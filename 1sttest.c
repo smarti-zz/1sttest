fde5530cf6532febace716e865c948c3ead44131
smarti-zz <div__@t-online.de>
2021-01-05 14:59:25 +0100

    Create 1sttest.c

---
+++ 1sttest.c
@@ -0,0 +1,15 @@
+//1sttest.c msp 210104
+//Simple c-code for printing command-line parameters
+#include <stdio.h>
+
+int main(int arg c, char *argv[]) {
+  int i;
+  char **sp = argv;
+  
+  for (i = 0; i < argc; i++)
+    printf("argv[%d] = %s\n", i, *sp++);
+  
+  puts("Ready!")
+  
+  return 0;
+}
